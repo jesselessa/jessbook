229dbbb6b4ea256f8918a54a363cc6cf7dbaa127
jesselessa <jessica.elessa@gmail.com>
2023-08-03 01:30:21 +0200

    completed comments feature client side

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -1,5 +1,8 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./comments.scss";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { makeRequest } from "../../utils/axios.jsx";
+import moment from "moment";
 
 // Contexts
 import { AuthContext } from "../../contexts/authContext.jsx";
@@ -10,9 +13,32 @@ import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 export default function Comments({ postId }) {
   const { currentUser } = useContext(AuthContext);
 
-  // TODO : Complete the below function
-  const handleSubmit = (e) => {
+  const [desc, setDesc] = useState("");
+
+  const { isLoading, error, data } = useQuery(["comments"], () =>
+    makeRequest.get(`/comments?postId=${postId}`).then((res) => {
+      return res.data;
+    })
+  );
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (newComment) => {
+      return makeRequest.post("/comments", newComment);
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries(["comments"]);
+      },
+    }
+  );
+
+  const handleClick = async (e) => {
     e.preventDefault();
+    mutation.mutate({ desc, postId });
+    setDesc("");
   };
 
   return (
@@ -20,36 +46,47 @@ export default function Comments({ postId }) {
       <form>
         <div className="img-container">
           <img src={currentUser.profilePic} alt="user" />
+          {/* <img src={`/uploads/${currentUser.profilePic}`} alt="user" /> */}
         </div>
 
         <div className="inputGroup">
-          <input type="text" placeholder="Write a comment..." />
+          <input
+            type="text"
+            placeholder="Write a comment..."
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
           <SendOutlinedIcon
             className="send"
             sx={{ fontSize: "24px", color: "#333" }}
-            onClick={handleSubmit}
+            onClick={handleClick}
           />
         </div>
 
-        <button type="submit" onClick={handleSubmit}>
-          Send
-        </button>
+        <button onClick={handleClick}>Send</button>
       </form>
 
-      {comments.map((comment) => (
-        <div className="comment" key={comment.id}>
-          <div className="img-container">
-            <img src={comment.profilePic} alt="user" />
-          </div>
-          <div className="info">
-            <h3>
-              {comment.firstName} {comment.lastName}
-            </h3>
-            <p>{comment.desc}</p>
-          </div>
-          <span className="time">1 hour ago</span>
-        </div>
-      ))}
+      {error
+        ? "Something went wrong"
+        : isLoading
+        ? "Loading..."
+        : data.map((comment) => (
+            <div className="comment" key={comment.id}>
+              <div className="img-container">
+                <img src={comment.profilePic} alt="user" />
+                {/* <img src={`/uploads/${comment.profilePic}`} alt="user" /> */}
+              </div>
+              <div className="info">
+                <h3>
+                  {comment.firstName} {comment.lastName}
+                </h3>
+                <p>{comment.desc}</p>
+              </div>
+              <span className="time">
+                {moment(comment.creationDate).fromNow()}
+              </span>
+            </div>
+          ))}
     </div>
   );
 }
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -62,7 +62,7 @@ export default function Post({ post }) {
         </div>
       </div>
 
-      {commentsOpen && <Comments />}
+      {commentsOpen && <Comments postId={post.id} />}
     </div>
   );
 }
