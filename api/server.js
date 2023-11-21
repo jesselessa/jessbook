58dae815d797c6fa6f8031fba3fbec927d62d960
jesselessa <jessica.elessa@gmail.com>
2023-11-21 14:54:52 +0100

    changed server message when starting

--- api/server.js
+++ api/server.js
@@ -50,5 +50,5 @@ app.post("/uploads", upload, (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server listening at http://${HOST}:${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
