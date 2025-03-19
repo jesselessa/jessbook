60760dbb0bbb114bfd562ad59b2752879bb1c59c
jesselessa <jessica.elessa@gmail.com>
2025-03-19 03:45:05 +0100

    installed mysql2 instead of mysql and changed former database configuration

--- api/utils/connect.js
+++ api/utils/connect.js
@@ -1,14 +1,28 @@
-import mysql from "mysql";
+import mysql from "mysql2/promise";
+import "dotenv/config";
 
 // Configure database
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PSWD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((error) => {
-  if (error) throw new Error(error.message);
-  console.log("Database connected to server");
-});
+// Test database connection and manage errors
+async function testConnection() {
+  try {
+    const [rows, fields] = await db.query("SELECT 1");
+    console.log("Database connected to server");
+  } catch (error) {
+    console.error("Database connection error:", error.message);
+    throw new Error(
+      "Failed to connect to database. Application cannot start."
+    );
+  }
+}
+
+testConnection();
