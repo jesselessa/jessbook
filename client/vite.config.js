5c3c4639c942bf36b01d54232d6787e4a31690c8
jesselessa <jessica.elessa@gmail.com>
2025-03-21 05:43:24 +0100

    deleted most photos of 'upload folder'

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: "[name].[hash].js",
-      },
-    },
-  },
 });
